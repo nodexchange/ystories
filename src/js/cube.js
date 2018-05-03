162ec58a90c6808021b7cfc12511025d9700ecdf
Martin Wojtala (Win10)- Master JavaScript Blaster <martin.wojtala@oath.com>
2018-05-03 17:16:48 +0100

    script

--- src/js/cube.js
+++ src/js/cube.js
@@ -6,12 +6,42 @@ class Cube {
     this.sides.left = this.cubeRoot.querySelector('--left');
     this.sides.right = this.cubeRoot.querySelector('--right');
     this.sides.back = this.cubeRoot.querySelector('--back');
+    this.currentRotation = 0;
     console.log(this.sides);
+    
+    setTimeout(() => {
+      console.log('________');
+      this.rotateLeft();
+    }, 1000);
     /*
     _3dface--front
     _3dface--left
     _3dface--right
     _3dface--back
     */
+  }
+
+  rotateLeft() {
+    this.animateCube('left');
+  }
+  
+  rotateRight() {
+    this.animateCube('right');
+  }
+  
+  animateCube(direction) {
+    let slightOffset = 0;
+    if (direction === 'right') {
+      slightOffset = this.currentRotation + 20;
+      this.currentRotation += 90;
+    } else {
+      slightOffset = this.currentRotation - 20;
+      this.currentRotation -= 90;
+    }
+    console.log(' slightOffset >>> ' + slightOffset);
+    this.cubeRoot.style.transform = 'rotateY(' + slightOffset + 'deg) scale(0.8);';
+    setTimeout(() => {
+      // this.cubeRoot.style.transform = 'rotateY(' + this.currentRotation + 'deg) scale(1)';
+    }, 1300);
   }
 }
